b4f31c54ea3f6e0454701472293bc955cb39ecb4
jsnvllnv <84897955+jsnvllnv@users.noreply.github.com>
2022-05-13 19:15:48 +0800

    Update extract_clean_csat.sql

--- extract_clean_csat.sql
+++ extract_clean_csat.sql
@@ -2,17 +2,17 @@ select
 	ticket_id,
 	channel,
 	to_char(local_date_created::date,'mm/dd/yyyy') as local_date_created,
-	local_time_created,
+	extract(hour from local_time_created::time) as hour_,
 	satisfaction_rating_score,
 	satisfaction_rating_comment,
 	tags,
 	type,
-	trunc(uncontrollable_dsat::numeric)::varchar,
-	zendesk_assignee 
+	trunc(uncontrollable_dsat::numeric)::varchar as uncontrollable_dsat,
+	zendesk_assignee,
+	subteam 
 from 
 		(select 
-			*,
-			row_number() over (partition by ticket_id, local_date_created,zendesk_assignee) as r1
+			*
 		from 
 			(select 
 				distinct 
@@ -26,7 +26,10 @@ from
 				type,
 			--	id_,
 				ext_interaction_id as uncontrollable_dsat,
-				agent_.agent_name as zendesk_assignee
+				agent_.agent_name as zendesk_assignee,
+				date_updated,
+				min(date_updated) over (partition by ticket_id) as last_update,
+				subteam
 					from
 						(
 							select 
@@ -38,7 +41,8 @@ from
 								satisfaction_rating_comment,
 								id_,
 								tags,
-								type
+								type,
+								date_updated
 									from
 											(
 												select 
@@ -55,7 +59,8 @@ from
 																	satisfaction_rating_comment,
 																	assignee_id as id_,
 																	tags,
-																	type
+																	type,
+																	date_updated
 																from 
 																	zendesk_email_tickets zet
 																union all 
@@ -69,7 +74,8 @@ from
 																	satisfaction_rating_comment,
 																	assignee_id as id_,
 																	tags,
-																	type
+																	type,
+																	date_updated
 																from 
 																	zendesk_chat_tickets zct 
 																union all 
@@ -83,7 +89,8 @@ from
 																	satisfaction_rating_comment,
 																	assignee_id as id_,
 																	tags,
-																	type
+																	type,
+																	date_updated
 																from 
 																	zendesk_call_tickets zct2 
 																union all 
@@ -97,7 +104,8 @@ from
 																	satisfaction_rating_comment,
 																	assignee_id as id_,
 																	tags,
-																	type
+																	type,
+																	date_updated
 																from 
 																	zendesk_helpdesk_tickets zht 
 													) as all_tickets
@@ -111,21 +119,24 @@ from
 				left join 
 					(
 						select 
-							assignee_id,
-							agent_name 
+							agent_name,
+							subteam,
+							agent_name as name_, 
+							start_date,
+							end_date,
+							zendesk_id
 						from 
-							sd_agent_roster sar 
-						where 
-							client_account = 'urbanstems_seasonal'
-							or client_account = 'urbanstems'
-							or client_account = 'urbanstems_flex'
-					) as agent_ on assignee_id::varchar = id_::varchar
+							sd_subteam_urbanstems ssu 
+						where is_core_team is true and subteam not like '%tc%' and subteam not like '%careops%'
+					) as agent_ on (zendesk_id::varchar = id_::varchar and local_date_created between start_date and end_date)
 			left join 
 				t_dsat_urbanstems tdu on trunc(ext_interaction_id::numeric)::varchar = csat_.ticket_id::varchar 
 				where 
-					agent_.agent_name is not null and local_date_created >= '2022-01-01'
+					agent_.agent_name is not null and 
+					local_date_created::date >= '2022-01-01'
 				order by local_date_created
-			) as final_csat	) as duplicates 
-		where 
-			r1 = 1 and uncontrollable_dsat is null 
+			) as final_csat 
+		where date_updated = last_update	
+		) as duplicates 
+	where uncontrollable_dsat is null
 order by local_date_created, local_time_created, ticket_id 
